bbbd726777c9b951ec778dbc3e508a9e88ebb7c0
Lukáš Tůma <lukas.tuma@outlook.cz>
2021-04-11 08:40:00 +0200

    statistiky

--- Script.py
+++ Script.py
@@ -1,8 +1,18 @@
 import sqlalchemy
 import pandas as pd
+import numpy as np
 
 conn_string = f"mysql+pymysql://student:p7@vw7MCatmnKjy7@data.engeto.com/data"
 alchemy_conn = sqlalchemy.create_engine(conn_string)
 
 df = pd.read_sql('edinburgh_bikes', alchemy_conn, parse_dates=True)
-print(df.head())
+
+print("Celkovy pocet vypujcek od '" + str(df['started_at'].min()).split()[0] + "' do '" + str(df['ended_at'].max()).split()[0] + "': " + str(len(df.index)))
+print("Prumerna delka trvani vypujcky: " + str(int(np.round(df['duration'].mean() / 60))) + " min a " + str(int(((df['duration'].mean() / 60) % 1) * 60)) + " sec")
+
+start_stations_ids_arr = np.unique(df['start_station_id'].to_numpy())
+end_stations_ids_arr = np.unique(df['end_station_id'].to_numpy())
+print("Celkovy pocet stanic: " + str(np.unique(np.concatenate((start_stations_ids_arr,end_stations_ids_arr))).shape[0]))
+
+mask = ~np.isin(start_stations_ids_arr, end_stations_ids_arr)
+print(start_stations_ids_arr[mask])
